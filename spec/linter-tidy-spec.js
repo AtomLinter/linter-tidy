fdc5ef0977ece28c17592b3844835fefcad32353
Landon Abney <Landonabney@gmail.com>
2016-10-11 13:30:57 -0700

    Fix lint issues

--- spec/linter-tidy-spec.js
+++ spec/linter-tidy-spec.js
@@ -2,7 +2,7 @@
 
 import * as path from 'path';
 
-const lint = require(path.join('..', 'lib', 'main.coffee')).provideLinter().lint;
+const lint = require('../lib/main.coffee').provideLinter().lint;
 
 const badFile = path.join(__dirname, 'fixtures', 'bad.html');
 const goodFile = path.join(__dirname, 'fixtures', 'good.html');
@@ -22,7 +22,7 @@ describe('The Tidy provider for Linter', () => {
     let editor = null;
     beforeEach(() => {
       waitsForPromise(() =>
-        atom.workspace.open(badFile).then(openEditor => {
+        atom.workspace.open(badFile).then((openEditor) => {
           editor = openEditor;
         })
       );
@@ -30,45 +30,45 @@ describe('The Tidy provider for Linter', () => {
 
     it('finds at least one message', () => {
       waitsForPromise(() =>
-        lint(editor).then(messages => {
-          expect(messages.length).toBeGreaterThan(0);
-        })
+        lint(editor).then(messages =>
+          expect(messages.length).toBeGreaterThan(0)
+        )
       );
     });
 
     it('verifies the first message', () => {
-      waitsForPromise(() => {
-        const messageText = '<img> lacks "alt" attribute';
-        return lint(editor).then(messages => {
+      const messageText = '<img> lacks "alt" attribute';
+      waitsForPromise(() =>
+        lint(editor).then((messages) => {
           expect(messages[0].type).toBe('Warning');
           expect(messages[0].html).not.toBeDefined();
           expect(messages[0].text).toBe(messageText);
           expect(messages[0].filePath).toBe(badFile);
           expect(messages[0].range).toEqual([[6, 0], [6, 4]]);
-        });
-      });
+        })
+      );
     });
   });
 
   it('finds nothing wrong with a valid file', () => {
     waitsForPromise(() =>
       atom.workspace.open(goodFile).then(editor =>
-        lint(editor).then(messages => {
-          expect(messages.length).toBe(0);
-        })
+        lint(editor).then(messages =>
+          expect(messages.length).toBe(0)
+        )
       )
     );
   });
 
   it('finds errors on the fly', () => {
     waitsForPromise(() =>
-      atom.workspace.open(goodFile).then(editor => {
+      atom.workspace.open(goodFile).then((editor) => {
         editor.moveToBottom();
         editor.insertText('\n<h2>This should not be outside the body!</h2>\n');
         return lint(editor);
-      }).then(messages => {
-        expect(messages.length).toBeGreaterThan(0);
-      })
+      }).then(messages =>
+        expect(messages.length).toBeGreaterThan(0)
+      )
     );
   });
 });
